22dd3d233cda4083ee80c40433df8852c0f1d4b6
anthony.lifetech <anthony@lifetech.co.id>
2022-02-04 15:03:35 +0700

    New Changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
+const mysql2 = require('mysql2')
 
 const database = process.env.DB_NAME
 const username = process.env.DB_USERNAME
@@ -7,7 +8,7 @@ const password = process.env.DB_PASSWORD
 const db = new Sequelize(database, username, password, {
   host: 'localhost',
   dialect: 'mysql',
-  dialectModule: require('mysql2')
+  dialectModule: mysql2
 })
 
 module.exports = db
