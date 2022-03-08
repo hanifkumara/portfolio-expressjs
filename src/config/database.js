420fa17d138339e08e1d15d9f4d63d503c51e612
hanifkumara <hnflasting@gmail.com>
2022-03-08 20:25:53 +0700

    New Changes

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,10 @@ const mysql2 = require('mysql2')
 const database = process.env.DB_NAME
 const username = process.env.DB_USERNAME
 const password = process.env.DB_PASSWORD
+const host = process.env.DB_HOST
 
 const db = new Sequelize(database, username, password, {
-  host: 'localhost',
+  host: host,
   dialect: 'mysql',
   dialectModule: mysql2
 })
